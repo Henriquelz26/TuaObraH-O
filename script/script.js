5b4a28898e489b12c0b3b922f7a37d0bde4a4807
Henrique <henriquelenz0712@gmail.com>
2025-02-09 22:43:40 -0300

    upando json

--- script/script.js
+++ script/script.js
@@ -1,36 +1,14 @@
 
-function salvarDados() {
-    const nome = document.getElementById("inputnome").value;
-    const senha = document.getElementById("inputsenha").value;
-    const email = document.getElementById("inputemail").value;
-    const cpf = document.getElementById("inputcpf").value;
-    const cidade = document.getElementById("inputcidade").value;
-    const endereco = document.getElementById("inputendereco").value;
 
-    const usuario = {
-        nome,
-        senha,
-        email,
-        cpf,
-        cidade,
-        endereco
-    };
+let users = document.querySelector("#infoUser")
 
-    // Envia os dados para o backend usando fetch
-    fetch("http://localhost:3000/salvar", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(usuario)
+
+fetch("../json/users.json").then((response) => {
+    response.json().then((users) => {
+        users.usuarios.map((user) =>{
+            infoUser.innerHTML +=`<ul><li>Nome: ${user.nome}</li><li>E-mail: ${user.email}</li><li>Cidade: ${user.cidade}</li><li>Descrição: ${user.descricao}</li></ul>` 
+
+        })
     })
-    .then(response => response.json())
-    .then(data => {
-        alert(data.mensagem); // Exibe mensagem de sucesso
-        document.getElementById("userForm").reset(); // Limpa o formulário
-    })
-    .catch(error => {
-        console.error("Erro:", error);
-        alert("Ocorreu um erro ao salvar os dados.");
-    });
-}
+
+})
